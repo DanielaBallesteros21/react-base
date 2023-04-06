7c0d963e31f803eba791533b52668d83dd899c69
DanielaBallesteros21 <daniela.ferballesteros98@gmail.com>
2023-04-06 17:35:48 -0500

    plantilla react

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+import Layout from './layout/Layout'
+import { Page } from './pages'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+function App() {
+  let AppPage = (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout><Page/></Layout>}></Route>
+      </Routes>
+    </Router>
+  )
+
+  return AppPage
+}
+
+export default App
